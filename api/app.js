71c52b1d16aa8cc477adda82a193a5e586ec12df
MarchanoGG <56337567+MarchanoGG@users.noreply.github.com>
2025-03-23 21:25:44 +0100

    PUT /books/{id} Een bestaand boek bijwerken

--- api/app.js
+++ api/app.js
@@ -48,7 +48,7 @@ app.get('/books/:id', (req, res) => {
 });
 
 
-// // === POST ===
+// === POST ===
 
 // POST /books: Een nieuw boek toevoegen
 app.post('/books', (req, res) => {
@@ -57,6 +57,20 @@ app.post('/books', (req, res) => {
     res.status(201).json(newBook);
 });
 
+
+// === PUT ===
+
+// PUT /books/{id} Een bestaand boek bijwerken
+app.put('/books/:id', (req, res) => {
+    const bookId = parseInt(req.params.id);
+    const index = books.findIndex(b => b.id === bookId);
+    if (index !== -1) {
+        books[index] = req.body;
+        res.status(200).json(books[index]);
+    } else {
+        res.status(404).json({ message: "Book not found" });
+    }
+});
 
 
 // Start server
